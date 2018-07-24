be1364d49bb53466eb9a3ae11c37b054dfa34823
朱勋杰（zhuxunjie01_sx）-技术产品中心 <zhuxunjie01_sx@qiyi.com>
2018-07-24 19:52:29 +0800

    remove redundant annotation

--- src/train.py
+++ src/train.py
@@ -183,18 +183,7 @@ def init_optimizer_criterion(model, opt):
     :param opt:
     :return:
     """
-    '''
-    if not opt.copy_attention:
-        weight_mask = torch.ones(opt.vocab_size).cuda() if torch.cuda.is_available() else torch.ones(opt.vocab_size)
-    else:
-        weight_mask = torch.ones(opt.vocab_size + opt.max_unk_words).cuda() if torch.cuda.is_available() else torch.ones(opt.vocab_size + opt.max_unk_words)
-    weight_mask[opt.word2id[pykp.IO.PAD_WORD]] = 0
-    criterion = torch.nn.NLLLoss(weight=weight_mask)
 
-    optimizer = Adam(params=filter(lambda p: p.requires_grad, model.parameters()), lr=opt.learning_rate)
-    # optimizer = torch.optim.Adadelta(model.parameters(), lr=0.1)
-    # optimizer = torch.optim.RMSprop(model.parameters(), lr=0.1)
-    '''
     criterion = torch.nn.NLLLoss(ignore_index=PAD_WORD)
 
     if opt.train_ml:
