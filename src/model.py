Fixed bugs in reward calculation

src/model.py
@@ -530,8 +530,14 @@ class Seq2SeqLSTMAttention(nn.Module):
         Word Sampling
             (1) Feedforwarding RNN
         '''
-        # take the first word (should be BOS <s>) of each target sequence (batch_size, 1)
-        trg_input = trg_inputs[:, 0].unsqueeze(1)
+        if sampling == "teacher_forcing":
+            trg_input = trg_inputs[:, 0].unsqueeze(1)
+        else:
+            #  the first word should be SOS <s>
+            sos_batch = np.full((batch_size, 1), SOS, dtype=np.int64)
+            trg_input = torch.from_numpy(sos_batch)
+            trg_input = trg_input.cuda() if torch.cuda.is_available() else trg_input
+
         decoder_log_probs = []
         attn_weights = []
         copy_weights = []
@@ -616,9 +622,8 @@ class Seq2SeqLSTMAttention(nn.Module):
                 categorical_distribution = Categorical(
                     logits=decoder_log_prob.data)
                 top_idx = categorical_distribution.sample()
-                predicted_indices.append(top_idx.unsqueeze(1))
+                predicted_indices.append(top_idx)
                 top_idx[top_idx >= self.vocab_size] = self.unk_word
-                top_idx = top_idx.unsqueeze(1)
                 # top_idx and next_index are (batch_size, 1)
                 trg_input = top_idx.cuda() if torch.cuda.is_available() else top_idx
 

src/train.py
@@ -72,6 +72,9 @@ def calculate_reward(predicted_indices, target_indices):
     target_mask = (target_indices != 0).data.numpy()
     predicted_seq_length = (predicted_indices == EOS).data.numpy()
     predicted_seq_length = np.argmax(predicted_seq_length, axis=1)
+    predicted_seq_length = np.where(predicted_seq_length == 0, max_length,
+                                    predicted_seq_length)
+
     predicted_mask = [np.concatenate((np.ones(ele), np.zeros(max_length - ele)))
                       for ele in predicted_seq_length]
     predicted_mask = np.array(predicted_mask)
@@ -82,7 +85,9 @@ def calculate_reward(predicted_indices, target_indices):
     target_seq_length = np.sum(target_mask, axis=1)
     precision = true_positive / predicted_seq_length
     recall = true_positive / target_seq_length
-    f1_score = 2 * precision * recall / (precision + recall)
+    numerator = 2 * precision * recall
+    denominator = precision + recall
+    f1_score = np.where(numerator == 0, 0, numerator / denominator)
 
     return np.average(f1_score)
 