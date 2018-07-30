88c6ba654851ce63903281a7c35e7dbd53d4d8ba
朱勋杰（zhuxunjie01_sx）-技术产品中心 <zhuxunjie01_sx@qiyi.com>
2018-07-30 11:36:17 +0800

    remove rubundant parameters

--- run.sh
+++ run.sh
@@ -3,4 +3,3 @@ python3 train.py --training-path "/home/zxj/Downloads/sorted_result_test.txt"  \
 --model-path "/home/zxj/Downloads" \
 --num-epoches 1000 \
 --run-valid-every 10 --save-model-every 200000 \
---num-epoches 1000
