ef3e8903618ddc22dcc70c54d075910c7fa840d2
朱勋杰（zhuxunjie01_sx）-技术产品中心 <zhuxunjie01_sx@qiyi.com>
2018-07-30 11:36:01 +0800

    change running epoches

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 python3 train.py --training-path "/home/zxj/Downloads/sorted_result_test.txt"  \
 --batch-size 128 \
 --model-path "/home/zxj/Downloads" \
+--num-epoches 1000 \
 --run-valid-every 10 --save-model-every 200000 \
 --num-epoches 1000
