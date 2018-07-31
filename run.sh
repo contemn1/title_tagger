c2028d76249e1b5af190ae3e75ec5ef2f7d4fe90
朱勋杰（zhuxunjie01_sx）-技术产品中心 <zhuxunjie01_sx@qiyi.com>
2018-07-31 11:29:57 +0800

    add more parameters to running script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 python3 train.py --training-path "${DATA_DIR}"  \
---batch-size 128 \
---model-path "/home/zxj/Downloads" \
+--training-file "filtered_sorted_result.txt" \
+--batch-size 64 \
+--rnn-size 64 \
+--word-vec-size 64 \
+--model-path "${OUTPUT_DIR}" \
 --num-epoches 1000 \
 --run-valid-every 10 --save-model-every 20000 \
