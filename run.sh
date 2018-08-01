e6a456f54315a2eb110503a095af819f0ffc67ee
朱勋杰（zhuxunjie01_sx) <zhuxunjie01_sx@qiyi.com>
2018-08-01 18:09:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 python train.py --training-dir "${DATA_DIR}"  \
 --training-file "filtered_sorted_result.txt" \
---batch-size 128 \
+--batch-size 256 \
 --rnn-size 64 \
 --word-vec-size 64 \
 --model-path "${OUTPUT_DIR}" \
 --num-epoches 1000 \
---run-valid-every 20000 --save-model-every 40000
+--run-valid-every 20000 --save-model-every 40000 \
+--print-loss-every 200
