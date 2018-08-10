888dd8189d09298578cf3d62adf87321177b18e5
朱勋杰（zhuxunjie01_sx）-技术产品中心 <zhuxunjie01_sx@qiyi.com>
2018-08-10 18:09:49 +0800

    add dropout

--- src/model.py
+++ src/model.py
@@ -154,6 +154,7 @@ class CopyDecoder(nn.Module):
         )
 
         self.softmax = nn.LogSoftmax(dim=-1)
+        self.dropout = nn.Dropout(dropout)
         self.tanh = nn.Tanh()
 
         self.decoder2vocab = nn.Linear(decoder_hidden * 3, self.vocab_size)
@@ -240,6 +241,8 @@ class CopyDecoder(nn.Module):
 
             decoder_logit = torch.cat((decoder_output, enc_dec_attention,
                                        dec_self_attention), 2)
+
+            decoder_logit = self.dropout(decoder_logit)
             generation_prob_dist = self.decoder2vocab(decoder_logit)
 
             copy_prob = self.copy_switch(decoder_logit)
