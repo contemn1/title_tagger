1225c9d4cffd3aa6c6a34f9340fda6327bdec5da
朱勋杰（zhuxunjie01_sx）-技术产品中心 <zhuxunjie01_sx@qiyi.com>
2018-08-14 22:29:21 +0800

    change parameter

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 python train.py --training-dir "${DATA_DIR}"  \
---training-file "result_20170321_20171231_filtered_v2.txt" \
+--training-file "result_20170321_20171231_filtered_v3.txt" \
 --batch-size 128 \
 --rnn-size 64 \
 --word-vec-size 64 \
 --model-path "${OUTPUT_DIR}" \
 --num-epoches 1000 \
 --run-valid-every 10000 --save-model-every 20000 \
---early-stop-tolerance 30 \
+--early-stop-tolerance 40 \
 --print-loss-every 200 --normalize-attention True \
---dropout 0.5 --word-index-map-name "word_to_index_v2.txt" \
---tag-index-map-name "tag_to_index_v2.txt" --store-dict True
+--dropout 0.5 --word-index-map-name "word_to_index_v3.txt" \
+--tag-index-map-name "tag_to_index_v3.txt" --store-dict True
