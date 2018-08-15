4f05b8dd1761907572f0f3bca3182394307ced16
朱勋杰（zhuxunjie01_sx）-技术产品中心 <zhuxunjie01_sx@qiyi.com>
2018-08-15 14:54:34 +0800

    add restore parameter to run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,30 @@
-python train.py --training-dir "${DATA_DIR}"  \
---training-file "result_20170321_20171231_filtered_v3.txt" \
---batch-size 128 \
---rnn-size 64 \
---word-vec-size 64 \
---model-path "${OUTPUT_DIR}" \
---num-epoches 1000 \
---run-valid-every 10000 --save-model-every 20000 \
---early-stop-tolerance 45 \
---print-loss-every 200 --normalize-attention True \
---dropout 0.5 --word-index-map-name "word_to_index_v3.txt" \
---tag-index-map-name "tag_to_index_v3.txt" --store-dict True
+if [ ${PREVIOUS_JOB_OUTPUT_DIR} ]; then
+    python train.py --training-dir "${DATA_DIR}"  \
+    --training-file "result_20170321_20171231_filtered_v2.txt" \
+    --batch-size 128 \
+    --rnn-size 64 \
+    --word-vec-size 64 \
+    --model-path "${OUTPUT_DIR}" \
+    --num-epoches 1000 \
+    --run-valid-every 10000 --save-model-every 20000 \
+    --early-stop-tolerance 45 \
+    --print-loss-every 200 --normalize-attention True \
+    --dropout 0.5 --word-index-map-name "word_to_index_v2.txt" \
+    --tag-index-map-name "tag_to_index_v2.txt" --store-dict False \
+    --previous-output-dir ${PREVIOUS_JOB_OUTPUT_DIR} \
+    --restore-model True
+
+else
+    python train.py --training-dir "${DATA_DIR}"  \
+    --training-file "result_20170321_20171231_filtered_v2.txt" \
+    --batch-size 128 \
+    --rnn-size 64 \
+    --word-vec-size 64 \
+    --model-path "${OUTPUT_DIR}" \
+    --num-epoches 1000 \
+    --run-valid-every 10000 --save-model-every 20000 \
+    --early-stop-tolerance 45 \
+    --print-loss-every 200 --normalize-attention True \
+    --dropout 0.5 --word-index-map-name "word_to_index_v2.txt" \
+    --tag-index-map-name "tag_to_index_v2.txt" --store-dict True \
+
