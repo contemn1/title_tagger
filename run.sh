a22726c872051d25cb39ce9519e153443ada1ce4
朱勋杰（zhuxunjie01_sx）-技术产品中心 <zhuxunjie01_sx@qiyi.com>
2018-08-16 22:16:06 +0800

    update run

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 if [ ${PREVIOUS_JOB_OUTPUT_DIR} ]; then
     python train.py --training-dir "${DATA_DIR}"  \
-    --training-file "result_20170321_20171231_filtered_v2.txt" \
+    --training-file "result_20170321_20171231_filtered_v3.txt" \
     --batch-size 128 \
     --rnn-size 64 \
     --word-vec-size 64 \
@@ -9,14 +9,14 @@ if [ ${PREVIOUS_JOB_OUTPUT_DIR} ]; then
     --run-valid-every 10000 --save-model-every 20000 \
     --early-stop-tolerance 45 \
     --print-loss-every 200 --normalize-attention True \
-    --dropout 0.5 --word-index-map-name "word_to_index_v2.txt" \
-    --tag-index-map-name "tag_to_index_v2.txt" --store-dict False \
+    --dropout 0.5 --word-index-map-name "word_to_index_v3.txt" \
+    --tag-index-map-name "tag_to_index_v3.txt" --store-dict False \
     --previous-output-dir ${PREVIOUS_JOB_OUTPUT_DIR} \
     --restore-model True
 
 else
     python train.py --training-dir "${DATA_DIR}"  \
-    --training-file "result_20170321_20171231_filtered_v2.txt" \
+    --training-file "result_20170321_20171231_filtered_v3.txt" \
     --batch-size 128 \
     --rnn-size 64 \
     --word-vec-size 64 \
@@ -25,6 +25,6 @@ else
     --run-valid-every 10000 --save-model-every 20000 \
     --early-stop-tolerance 45 \
     --print-loss-every 200 --normalize-attention True \
-    --dropout 0.5 --word-index-map-name "word_to_index_v2.txt" \
-    --tag-index-map-name "tag_to_index_v2.txt" --store-dict True
+    --dropout 0.5 --word-index-map-name "word_to_index_v3.txt" \
+    --tag-index-map-name "tag_to_index_v3.txt" --store-dict True
 fi
