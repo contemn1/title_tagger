5cb14f58c07fd087ea3c4b7c41d1ffa2ee544d0a
朱勋杰（zhuxunjie01_sx）-技术产品中心 <zhuxunjie01_sx@qiyi.com>
2018-08-17 18:22:38 +0800

    change sample

--- src/sampling.py
+++ src/sampling.py
@@ -14,6 +14,7 @@ def teacher_forcing_sampler(final_distribution, trg_inputs, step,
 
 
 def greedy_sampler(final_distribution, *unused_args, **unused_kargs):
+    final_distribution[:, :, UNK] = -float("inf")
     top_v, top_idx = final_distribution.topk(1, dim=-1)
     top_idx = top_idx.squeeze(2)
     return top_idx, top_idx
